9be75b4d488cf7024a90825e4054845dcf003d85
Rishabh Gupta <168845849+CODEWITHRISHU2005@users.noreply.github.com>
2025-07-12 07:20:12 +0530

    add

--- src/main/java/com/CodeWithRishu/Spring_AI/OllamaController.java
+++ src/main/java/com/CodeWithRishu/Spring_AI/OllamaController.java
@@ -1,15 +1,20 @@
 package com.CodeWithRishu.Spring_AI;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,20 +23,15 @@ import java.util.Map;
 public class OllamaController {
 
     private final ChatClient chatClient;
+    private final EmbeddingModel embeddingModel;
+    private final VectorStore vectorStore;
 
     @Autowired
-    private EmbeddingModel embeddingModel;
-
-    public OllamaController(OllamaChatModel chatModel) {
+    public OllamaController(EmbeddingModel embeddingModel, OllamaChatModel chatModel, VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
         this.chatClient = ChatClient.create(chatModel);
+        this.embeddingModel = embeddingModel;
     }
-
-
-//    public OlIamaController(ChatClient.Builder builder) {
-//        this.chatClient = builder.defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
-//                .build();
-//
-//    }
 
     @GetMapping("/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message) {
@@ -77,4 +77,44 @@ public class OllamaController {
     public float[] getEmbedding(@RequestParam String text) {
         return embeddingModel.embed(text);
     }
+
+    @PostMapping("/similarity")
+    public ResponseEntity<Double> getSimilarity(@RequestParam String text1, @RequestParam String text2) {
+        float[] embedding1 = embeddingModel.embed(text1);
+        float[] embedding2 = embeddingModel.embed(text2);
+
+        double dotProduct = 0.0;
+        double norm1 = 0.0;
+        double norm2 = 0.0;
+
+        for (int i = 0; i < embedding1.length; i++) {
+            dotProduct += embedding1[i] * embedding2[i];
+            norm1 += Math.pow(embedding1[i], 2);
+            norm2 += Math.pow(embedding2[i], 2);
+        }
+
+        double cosineSimilarity = dotProduct * 100 / (Math.sqrt(norm1) * Math.sqrt(norm2));
+
+        return ResponseEntity.ok(cosineSimilarity);
+    }
+
+    @PostMapping("/product")
+    public List<Document> getProducts(@RequestParam String text) {
+        return vectorStore.similaritySearch(
+                SearchRequest.
+                        builder()
+                        .query(text)
+                        .topK(2)
+                        .build());
+    }
+
+    @PostMapping("/ask")
+    public String getAnswerUsingRag(@RequestParam String query) {
+        return chatClient
+                .prompt(query)
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .call()
+                .content();
+    }
+
 }
