cd727c6f8206fdf5a9753320f82e8f7c51493737
Rishabh Gupta <168845849+CODEWITHRISHU2005@users.noreply.github.com>
2025-08-12 11:18:19 +0530

    add

---
+++ src/main/java/com/CodeWithRishu/Spring_AI/chat/ChatController.java
@@ -0,0 +1,46 @@
+package com.CodeWithRishu.Spring_AI.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    @Value("classpath:sample-image.jpg")
+    private Resource sampleImage;
+
+    @Autowired
+    public ChatController(ChatClient.Builder chatClientBuilder,
+                          ChatMemory chatMemory) {
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+    }
+
+    @PostMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> chatWithStream(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content();
+    }
+}
